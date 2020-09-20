a48828cb3ea8f45601a0fb67971e1dc7b74a26aa
CI Action <ci@carrott.com>
2020-09-20 19:23:44 +0100

    fix: typo

--- api/routes/storage.js
+++ api/routes/storage.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { router } = require('..')
+const router = express.Router()
 
 const gethandler = require('../handlers/get')
 const postHandler = require('../handlers/post')
